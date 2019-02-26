f079e5c7b540c7c5c62050c0a162cb45ce1ddf79
kontrollanten <alexander@width.se>
2019-02-26 20:35:08 +0100

    tests(demo): add Marker

--- e2e/App.js
+++ e2e/App.js
@@ -1,12 +1,15 @@
 import { h } from 'preact';
-import { Map, TileLayer } from '../src';
+import { divIcon } from 'leaflet';
+import { Map, Marker, TileLayer } from '../src';
 import 'leaflet/dist/leaflet.css';
+import './styles.css';
 
 export default () => (
   <div>
     <h1>preact-leaflet</h1>
     <Map center={[59.3367, 18.0667]} style={{ height: '100vh' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker icon={divIcon()} position={[59.3367, 18.0667]} />
     </Map>
   </div>
 );
