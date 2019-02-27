83798cac7ca200be170f19b719ddff6006184d9d
kontrollanten <alexander@width.se>
2019-02-27 17:38:12 +0100

    chore(Demo): update with more examples

--- e2e/App.js
+++ e2e/App.js
@@ -1,4 +1,4 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { divIcon } from 'leaflet';
 import {
   Map, Marker, Polyline, TileLayer,
@@ -7,14 +7,66 @@ import 'leaflet/dist/leaflet.css';
 import './styles.css';
 import route from './long-route';
 
-export default () => (
-  <div>
-    <h1>preact-leaflet</h1>
-    <Map center={[59.3367, 18.0667]} style={{ height: '100vh' }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker icon={divIcon()} position={[59.3367, 18.0667]} />
-      <Marker icon={divIcon()} position={[63.8374896962485, 20.163206074534]} />
-      <Polyline positions={route} />
-    </Map>
-  </div>
-);
+const getRouteConfig = (hash) => {
+  switch (hash) {
+    case '#polyline':
+      return {
+        mapCenter: [63.83919, 20.15069],
+        markers: [
+          [59.3367, 18.0667],
+          [63.8374896962485, 20.163206074534],
+        ],
+        polylines: [
+          route,
+        ],
+        zoom: 5,
+      };
+    default:
+      return {
+        mapCenter: [59.3367, 18.0667],
+        markers: [],
+        polylines: [],
+        zoom: 10,
+      };
+  }
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getRouteConfig(window.location.hash);
+  }
+
+  componentDidMount() {
+    window.addEventListener('popstate', () => {
+      this.setState(getRouteConfig(window.location.hash));
+    });
+  }
+
+  render(props, {
+    mapCenter, markers, polylines, zoom,
+  }) {
+    return (
+      <div>
+        <h1>preact-leaflet in</h1>
+        <ul className="menu">
+          <li>
+            <a href="#simple">Show simple</a>
+          </li>
+          <li>
+            <a href="#polyline">Show polyline and markers</a>
+          </li>
+        </ul>
+        <Map center={mapCenter} style={{ height: '100%' }} zoom={zoom}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {markers.map(position => (
+            <Marker icon={divIcon()} position={position} />
+          ))}
+          {polylines.map(positions => (
+            <Polyline positions={positions} />
+          ))}
+        </Map>
+      </div>
+    );
+  }
+}
